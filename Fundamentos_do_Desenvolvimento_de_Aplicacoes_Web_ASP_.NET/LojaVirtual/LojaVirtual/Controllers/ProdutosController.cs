7d08840670a1e92a48f193883d63bc42aae10a82
José Ricardo Lima dos Santos Pereira <jose.lsantos@al.infnet.edu.br>
2021-04-28 23:49:48 -0300

    Primeira aula Introdução ao um novo projeto em MVC
    Primeira aula Introdução ao um novo projeto em MVC

---
+++ Fundamentos_do_Desenvolvimento_de_Aplicacoes_Web_ASP_.NET/LojaVirtual/LojaVirtual/Controllers/ProdutosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            return View();
+        }
+
+
+    }
+
+    class Calculadora
+    {
+        int Somar(int numeroA, int numeroB)
+        {
+        }
+    }
+
+    public class GerenciadoDeProduto
+    {
+        ProdutoCadastrado CadastrarProduto(string nome, decimal preco)
+        {
+            Produto produto = new Produto();
+            produto.Nome = nome;
+            produto.Preco = preco;
+
+        }
+    }
+    public class ProdutoCadastrado { }
+
+    public class Produto { public string Nome; public decimal Preco; }
+
+    public class Contato { public string Nome; public int Telefone; }
+
+    public class Cliente { }
+
+
+}
