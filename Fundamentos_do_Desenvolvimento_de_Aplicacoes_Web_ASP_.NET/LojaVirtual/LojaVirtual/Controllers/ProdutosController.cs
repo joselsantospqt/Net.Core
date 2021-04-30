49b3027bdbd4e83cf4ae39c986007c8975cca49b
José Ricardo Lima dos Santos Pereira <jose.lsantos@al.infnet.edu.br>
2021-04-29 22:06:43 -0300

    Segunda Aula, Criar uma controladora, Adicionar um item e listar ele
    Segunda Aula, Criar uma controladora, Adicionar um item e listar ele

--- Fundamentos_do_Desenvolvimento_de_Aplicacoes_Web_ASP_.NET/LojaVirtual/LojaVirtual/Controllers/ProdutosController.cs
+++ Fundamentos_do_Desenvolvimento_de_Aplicacoes_Web_ASP_.NET/LojaVirtual/LojaVirtual/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,39 +9,77 @@ namespace LojaVirtual.Controllers
 {
     public class ProdutosController : Controller
     {
+
+        [HttpGet]
         public ActionResult Cadastrar()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Listar()
+        {
+            GerenciadoDeProduto gerenciador = new GerenciadoDeProduto();
+
+            List<Produto> listaDeProdutos = gerenciador.ListarProdutos();
+
+            return View(listaDeProdutos);
+        }
+
+        [HttpPost]
+        public ActionResult ExecutarCadastroDeProduto(String nome, decimal preco)
+        {
+
+            GerenciadoDeProduto gerenciador = new GerenciadoDeProduto();
+
+            gerenciador.CadastrarProduto(nome, preco);
+
+            return RedirectToAction("ProdutoCadastrado");
+        }
+
+        [HttpGet]
+        public ActionResult ProdutoCadastrado()
         {
             return View();
         }
 
-
     }
 
-    class Calculadora
-    {
-        int Somar(int numeroA, int numeroB)
-        {
-        }
-    }
 
     public class GerenciadoDeProduto
     {
-        ProdutoCadastrado CadastrarProduto(string nome, decimal preco)
+        static List<Produto> listaDeProdutos = new List<Produto>();
+
+        public string CadastrarProduto(string nome, decimal preco)
         {
+            //var vDados= (Produto)Session["Produtos"];
+
             Produto produto = new Produto();
             produto.Nome = nome;
             produto.Preco = preco;
 
+            listaDeProdutos.Add(produto);
+
+            return "Produto Cadastrado";
+
         }
+
+        public List<Produto> ListarProdutos()
+        {
+
+            return listaDeProdutos;
+
+        }
+
+        void AlterarProduto() { }
+
+        void ExcluirProduto() { }
+
+
     }
     public class ProdutoCadastrado { }
 
-    public class Produto { public string Nome; public decimal Preco; }
+    public class Produto { public string Nome; public decimal Preco; public int Quantidade; }
 
     public class Contato { public string Nome; public int Telefone; }
 
